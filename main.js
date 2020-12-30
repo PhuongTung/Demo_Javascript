6a6638829c5fdc1a695e92ec802b8d1fc8e4bcb7
PhuongTung <60902006+PhuongTung@users.noreply.github.com>
2020-12-30 08:20:54 +0700

    Clear require

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const fetch = require('node-fetch')
-
 function fetchAvatarUrl(userId) { 
-
+  return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
 
 const result = fetchAvatarUrl(123)
